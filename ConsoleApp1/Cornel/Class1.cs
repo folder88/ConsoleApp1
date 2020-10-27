54010da30313c44b1d4d2bed3208abc9ed97dc5b
Cornel Gurin <you@example.comcornelgurin@gmail.com>
2020-10-27 10:19:07 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Cornel/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Cornel
+{
+    class Class1
+    {
+
+        static void method()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World4!");
+
+            method();
+        }
+
+        static void method()
+        {
+            Console.WriteLine("hello from method");
+        }
+    }
+}
