e5e741261bd16d64e164f62ed6586049429a51d1
samira khadem <samira.khademhoseini@gmail.com>
2020-10-27 11:18:43 +0100

    class samira created

---
+++ ConsoleApp1/Samira/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Samira
+{
+    class ClassSamira
+    {
+        public void myMethod()
+        {
+            Console.WriteLine("Hello from samira");
+        }
+    }
+}
