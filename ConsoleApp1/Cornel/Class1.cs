214088dbbf3aabcfdff3debfa7de198061977980
Cornel Gurin <you@example.comcornelgurin@gmail.com>
2020-10-27 15:34:11 +0100

    all exercises

--- ConsoleApp1/Cornel/Class1.cs
+++ ConsoleApp1/Cornel/Class1.cs
@@ -25,7 +25,7 @@ namespace ConsoleApp1.Cornel
 
         }
 
-        public void exercises_03()
+        public void exercise_03()
         {
             string name = "John Smith";
             int age = 35;
@@ -35,5 +35,90 @@ namespace ConsoleApp1.Cornel
 
             Console.WriteLine("Hi,my name is 0" + name + ".I am " + age + "years old and i am " + height + "tall.I like playing basketball.Sometimes,people ask me if it is " + acces + " that I had in school only " + gender + "grades.Of course,it is not:).");
         }
+
+        public void exercise_04()
+        {
+            var var_01 = 150;
+            var var_02 = 300;
+
+
+            Console.WriteLine("Enter your first number:");
+
+          
+            double num_01 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number:");
+            double num_02 = Convert.ToDouble(Console.ReadLine());
+
+            
+
+            var variable_01 = num_01 < 150;
+            var variable_02 = num_02 > 300;
+
+           Console.WriteLine(" Is the first value " + num_01 + " less that 150 " + variable_01);
+           Console.WriteLine(" Is the first value " + num_02 + " less that 300 " + variable_02);
+
+
+        }
+
+        public void exercise_05()
+        {
+            Console.WriteLine("Enter the distance:");
+
+
+            double distance_01 = Convert.ToDouble(Console.ReadLine());
+
+            double distance_02 = distance_01 * 0.6214;
+
+            Console.WriteLine("Your distance in miles is " + distance_02);
+
+        }
+
+        public void exercises_06()
+        {
+            Console.WriteLine("Enter the first number");
+            double num_01 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            double num_02 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the operation");
+
+            var operation = Console.ReadLine();
+
+            if(operation == "+")
+            {
+                Console.WriteLine(num_01 + num_02);
+            }
+            else if(operation == "-")
+            {
+                Console.WriteLine(num_01 - num_02);
+            }
+            else if (operation == "*")
+            {
+                Console.WriteLine(num_01 * num_02);
+            }
+            else if (operation == "/")
+            {
+                Console.WriteLine(num_01 / num_02);
+            }
+
+
+        }
+
+        public void exercise_07()
+        {
+            Console.WriteLine("Enter your weight");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter your height");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+
+            double bmi = weight / (height * height);
+
+            Console.WriteLine("Your bmi is " + bmi);
+        }
+
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,34 +11,7 @@ namespace ConsoleApp1
         {
 
 
-            class_cornel object_01 = new Cornel.class_cornel();
-
-            object_01.exercise_01();
-            object_01.exercise_02();
-            object_01.exercises_03();
-
-            ClassSamira newclass = new Samira.ClassSamira();
-            //newclass.firstTask();
-            //newclass.secondTask();
-            //newclass.thirdTask();
-            //newclass.forthTask();
-            //newclass.fifthTask();
-            //newclass.sixthTask();
-            newclass.seventhTask();
-
-
-
-
-
-
-            testOne one = new Kathi.testOne();
-            // one.taskOne();
-            // one.taskTwo();
-            // one.taskThree();
-            // one.taskFour();
-            // one.taskFive();
-            // one.taskSix();
-            // one.taskSeven();
+            
 
         }
 
